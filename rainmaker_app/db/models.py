207f1e1b6dbddf76c25f9df7e463c6b537b3cf26
hattb <hattwj@yahoo.com>
2013-09-25 15:10:52 -0700

    Update and rename model.py to models.py

---
+++ rainmaker_app/db/models.py
@@ -0,0 +1,8 @@
+from rainmaker_app.model.schema_migration import SchemaMigration
+from rainmaker_app.model.sync_path import SyncPath
+from rainmaker_app.model.my_file import MyFile
+from rainmaker_app.model.difference import Difference
+from rainmaker_app.model.file_version import FileVersion
+from rainmaker_app.model.sync_comparison import SyncComparison
+
+models_arr = [ SchemaMigration, MyFile, SyncPath, Difference, FileVersion, SyncComparison]
